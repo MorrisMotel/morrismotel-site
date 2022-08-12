4cea381ba7d4e4a17b3900a4cea373a0539e1aad
Cain <contact@morrismotel.com>
2022-08-11 21:53:05 -0400

    improvements improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 
 export default defineConfig({
   markdown: {
-    remarkPlugins: [ ],
+    remarkPlugins: [remarkReadingTime],
   },
-  site: "http://localhost:3000/",
+  site: 'http://localhost:3000/',
   integrations: [sitemap(), tailwind()],
 });
-
---
+++ src/data/footnav.ts
@@ -0,0 +1,22 @@
+export const footNavContents = [
+  {
+    title: 'Privacy',
+    slug: '/privacypolicy',
+  },
+  {
+    title: 'Contribute',
+    slug: '/contribute',
+  },
+  {
+    title: 'DMCA',
+    slug: 'https://tally.so/r/nPdGW5',
+  },
+  {
+    title: 'Discord',
+    slug: 'https://discord.gg/AMrV3yjC4y',
+  },
+  {
+    title: 'Matrix',
+    slug: 'https://matrix.to/#/#morrismotel:morrismotel.com',
+  },
+];
