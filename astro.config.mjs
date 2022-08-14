4dcc3dda1b40a9e23ce5c5261394688e705b835e
Cain <cain@morrismotel.com>
2022-08-14 16:24:05 -0400

    add iconbrew icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import { astroImageTools } from "astro-imagetools";
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 
 export default defineConfig({
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
   markdown: {
     remarkPlugins: ['remark-gfm', 'remark-smartypants', remarkReadingTime],
   },
   site: 'http://localhost:3000/',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), astroImageTools],
 });
