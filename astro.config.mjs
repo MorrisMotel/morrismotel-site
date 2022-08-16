acdaf9daa8c59a5b724376eb51f7f75c4dfa06a1
Cain <cain@morrismotel.com>
2022-08-15 23:36:15 -0400

    image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import { astroImageTools } from "astro-imagetools";
+import image from '@astrojs/image'
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 
 export default defineConfig({
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  },
   markdown: {
     remarkPlugins: ['remark-gfm', 'remark-smartypants', remarkReadingTime],
   },
   site: 'http://localhost:3000/',
-  integrations: [sitemap(), tailwind(), astroImageTools],
+  integrations: [sitemap(), tailwind(), image()],
 });
