bb62fa0cebf2fbc9371a7f5832eeed1ee3caf849
Cain <cain@morrismotel.com>
2022-09-17 22:19:15 -0400

    use mdx for privacy policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: ['remark-gfm', 'remark-smartypants', remarkReadingTime],
+    remarkPlugins: ['remark-gfm', 'remark-smartypants', remarkReadingTime]
   },
   site: 'http://localhost:3000/',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), mdx()]
 });
