75ada262e75a88858506978e72f8ee3d8de26cfe
Cain <cain@morrismotel.com>
2022-09-18 16:09:30 -0400

    fix markdown plugin config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 import mdx from "@astrojs/mdx";
 
+import remarkGFM from "remark-gfm"
+import remarkSmartypants from "remark-smartypants"
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: ['remark-gfm', 'remark-smartypants', remarkReadingTime]
+    remarkPlugins: [remarkGFM, remarkSmartypants, remarkReadingTime]
   },
   site: 'http://localhost:3000/',
   integrations: [sitemap(), tailwind(), mdx()]
