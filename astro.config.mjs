989b32acabe0baf2f5664fd659a55a3f6fad693c
Cain <cain@morrismotel.com>
2023-01-06 20:17:50 -0500

    optimize article/hero images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from '/src/lib/remark-reading-time.mjs';
-
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true
   },
   site: 'http://localhost:3000/',
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), mdx(), image()]
 });
